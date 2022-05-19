e27c03fd199de15fc41a0d727e8f41a0e541c9be
MuhammadNabeelKhan0fficial <nabeelnadeem72360@gmail.com>
2022-05-20 03:51:18 +0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import { MoralisProvider } from 'react-moralis'
+import { NabeezonProvider } from '../context/NabeezonContext'
+import { ModalProvider } from 'react-simple-hook-modal'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER}
+      appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
+    >
+      <NabeezonProvider>
+        <ModalProvider>
+          <Component {...pageProps} />
+        </ModalProvider>
+      </NabeezonProvider>
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
